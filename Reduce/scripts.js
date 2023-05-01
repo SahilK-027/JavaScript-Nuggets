fccb70d2c9620131d6f3b45eb673590a1baabe1c
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2023-05-01 15:36:33 +0530

    ✨ Array reduce method

---
+++ Reduce/scripts.js
@@ -0,0 +1,13 @@
+const staff = [
+    {name: 'sk', salary: 2000},
+    {name: 'ak', salary: 1000},
+    {name: 'jk', salary: 800},
+    {name: 'pk', salary: 3000},
+];
+
+const montlySalaryTotal = staff.reduce((total, person)=>{
+    total += person.salary;
+    return total;
+}, 0) // This zero indicates the total will be initially 0
+
+console.log(montlySalaryTotal);
